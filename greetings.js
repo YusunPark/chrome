cbe3313717a33391fbf5a7cd7bcabcae870e2963
Yusun <solarpy@hanyang.ac.kr>
2021-06-07 23:22:54 +0900

    Update localstorage

--- greetings.js
+++ greetings.js
@@ -6,6 +6,24 @@ text = document.querySelector("h4");
 const USER_LS = "currentUser";
 const SHOWING_CN = "showing";
 
+function saveName(text) {
+  localStorage.setItem(USER_LS, text)
+
+}
+
+function handleSumit(event) {
+  event.preventDefault();
+  const currentValue = input.value;
+  paintGreeting(currentValue);
+  saveName(currentValue);
+}
+
+function askForName(text) {
+  form.classList.add(SHOWING_CN);
+  form.addEventListener("submit", handleSumit);
+  greeting.classList.remove(SHOWING_CN);
+}
+
 function paintGreeting(text) {
   form.classList.remove(SHOWING_CN);
   greeting.classList.add(SHOWING_CN);
@@ -16,7 +34,7 @@ function loadName() {
   const currentUser = localStorage.getItem(USER_LS);
   console.log(currentUser);
   if (currentUser === null) {
-    // paintGreeting(currentUser);
+    askForName(currentUser);
   } else {
     paintGreeting(currentUser);
   }
