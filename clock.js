fe0ced087d5628e42a1da42a1bdf9c817e4c0b4c
YusunPark <solarpy@hanyang.ac.kr>
2021-06-15 19:00:31 +0900

    Add countdown

--- clock.js
+++ clock.js
@@ -4,10 +4,10 @@ const clockTitle = clockContainer.querySelector("h1");
 
 // 현재 시각을 알려준다. 
 function getTime(){
-    const date = new Date();
-    const hours = date.getHours();
-    const minutes = date.getMinutes();
-    const seconds = date.getSeconds();
+    const Today = new Date();
+    const hours = 24 - Today.getHours();
+    const minutes = 60 - Today.getMinutes();
+    const seconds = 60 - Today.getSeconds();
     clockTitle.innerText = 
     `${hours < 10 ? `0${hours}` : hours }:${
         minutes < 10 ? `0${minutes}` : minutes}:${
